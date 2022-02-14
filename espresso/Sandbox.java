f9986c2682fe1127d8335b25fce1d5d087bae6f2
ceyhunalp <e.ceyhun.alp@gmail.com>
2022-02-14 12:33:52 +0100

    Updated Sandbox.java

--- espresso/Sandbox.java
+++ espresso/Sandbox.java
@@ -4,15 +4,31 @@ import java.lang.*;
 public class Sandbox{
 
         public static void main(String[] args) {
-                Contract c = new Contract(10);
-                //Contract c = new Contract();
-                //for (int i = 0; i < 1000000; i++) {
-                        //c.execute();
-                //}
+                int argCount = args.length;
+
+                if (argCount == 0) {
+                        System.out.println("Missing arguments");
+                        System.exit(0);
+                }
+
+                String opt = args[0];
+                Contract c;
+
+                if (opt.equals("-fp")) {
+                        c = new Contract(10);
+                } else {
+                        c = new Contract();
+                }
+                      
+                if (argCount < 2) {
+                        for (int i = 0; i < 1000000; i++) {
+                                c.execute();
+                        }
+                }
 
                 long start, end;
                 ArrayList<Long> execTimes = new ArrayList<Long>();
-                for (int i = 0; i < 1000; i++) {
+                for (int i = 0; i < 1000000; i++) {
                         start = System.nanoTime();
                         c.execute();
                         end = System.nanoTime();
